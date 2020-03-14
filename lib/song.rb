d66a36824132538fb797be5ed34a13592588f74f
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-14 19:49:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,31 @@ class Song
     self.class.all << self
   end
 
+  def self.create 
+    song = self.new 
+    song.save 
+    song
+  end
+    
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name 
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.create 
+    song.name = name
+    song 
+  end
+  
+  def self.find_by_name(name)
+    result = nil 
+    @@all.each do |song| 
+    result = song if song.name == name
+    end
+    result
+  end
+  
+  def self.find_or_create_by_name
 end
